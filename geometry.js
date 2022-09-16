a6aaee46ced51709e16800ff588140461cb37860
riowidywasa <riowidywasa@gmail.com>
2022-09-16 16:32:46 +0700

    Membuat subclass persegi dari class parent

--- geometry.js
+++ geometry.js
@@ -17,3 +17,34 @@ class Shape {
         throw new Error("Method 'draw()' must be implemented");
     }
 }
+
+class Persegi extends Shape {
+  constructor(sisi, nama, warna) {
+    super();
+    this.sisi = sisi;
+    this.nama = nama;
+    this.warna = warna;
+  }
+
+  luas() {
+    return this.sisi ** 2;
+  }
+
+  keliling() {
+    return this.sisi * 4;
+  }
+
+  draw() {
+    const bangun = document.getElementById("shape");
+    const newSquare = document.createElement('div');
+    newSquare.id = 'square';
+    bangun.appendChild(newSquare);
+    const text = document.createElement('h1');
+    text.innerHTML = this.nama;
+    bangun.append(text);
+    const square = document.getElementById('square');
+    square.style.width = this.sisi + 'px';
+    square.style.height = this.sisi + 'px';
+    square.style.background = this.warna;
+  }
+}
